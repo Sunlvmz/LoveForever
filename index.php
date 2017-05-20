692d88efc85c428eb83d2e1c5b9a960de66a6cc0
Sunlvmz <570066687@qq.com>
2017-05-20 11:02:50 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,312 @@
+<?php
+if (isset($_SERVER['HTTPS']) || '443' === $_SERVER['SERVER_PORT']) {
+    $url = 'https://';
+} else {
+    $url = 'http://';
+}
+$url = $url.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['DOCUMENT_URI']), '\\');
+$url = rtrim($url, '/');
+?><!DOCTYPE HTML>
+<!--[if IE 6]>
+<html id="ie6" class="ie ie6 lt-ie9">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" class="ie ie7 lt-ie9">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" class="ie ie8 lt-ie9">
+<![endif]-->
+<!--[if gte IE 9]>
+<html class="ie ie9">
+<![endif]-->
+<!--[if !(IE)]><!-->
+<html>
+<!--<![endif]-->
+<head>
+	<meta http-equiv='Pragma' content='no-cache' />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="author" content="Sun">
+	<meta name="description" content="For my lovely girl">
+	<title>For my lovely girl</title>
+	<link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+	<div id="nojavascript" class="nojavascript">您的浏览器版本太低或禁用了javascript，无法正常浏览本页面</div>
+	<script>document.getElementById('nojavascript').remove();</script>
+
+	<div class="flowtime">
+		<div class="ft-section section-1" data-id="section-1">
+			<div id="/section-1/page-1" class="ft-page page-1" data-id="page-1">
+				<p class="text1"><span id="text-75" contenteditable="true">Sun </span> ❤ <span id="text-76" contenteditable="true">Season</span> <span id="text-77" contenteditable="true">forever and ever</span></p>
+				<p class="text2"><span id="text-78" contenteditable="true">for my lovely girl</span></p>
+			</div>
+			<div id="/section-1/page-2" class="ft-page page-2" data-id="page-2">
+				<p class="top-text" id="text-1" contenteditable="true">是谁来自山川湖海，却囿于昼夜，厨房与爱</p>
+				<img src='img/iali24.jpg'  alt="过去的小明一直是一个人生活，享受着孤独，也憧憬着爱情。" />
+			</div>
+			<div id="/section-1/page-3" class="ft-page page-3 left-img" data-id="page-3">
+				<h2 class="text" id="text-2" contenteditable="true">唯愿在剩余光线面前，留下两眼为见你一面</h2>
+				<img src='img/iali1.jpg'  alt="一个人的长廊"/>
+			</div>
+			<div id="/section-1/page-4" class="ft-page page-4 full-img" data-id="page-4">
+				<h2 class="center-text" id="text-3" contenteditable="true">让我再看你一眼，星空和黑夜，西去而旋转的飞鸟</h2>
+				<img src='img/iali2.jpg'  alt="一个人的山岗" />
+			</div>
+		</div>
+		<div class="ft-section section-2" data-id="section-2">
+			<div id="/section-2/page-1" class="ft-page page-5 full-img" data-id="page-1">
+				<h2 class="center-text" id="text-4" contenteditable="true">争不过朝夕，又念着往昔，偷走了青丝却留住一个你</h2>
+				<img src='img/iali3.jpg'  alt="一个人的山岗" />
+			</div>
+			<div id="/section-2/page-2" class="ft-page page-6" data-id="page-2">
+				<h2 class="center-text" id="text-5" contenteditable="true">希望春天以后 你能成为那个你 我能成为那个我</h2>
+				<img src='img/iali4.jpg' alt="一个人的游乐场"/>
+			</div>
+			<div id="/section-2/page-3" class="ft-page page-7" data-id="page-3">
+				<h3 id="text-6" contenteditable="true">依然乐观，微笑着，等待着</h3>
+				<img src='img/iali5.jpg'  alt="但他依然乐观，微笑着，等待着"/>
+			</div>
+			<div id="/section-2/page-4" class="ft-page page-8 full-img" data-id="page-4">
+				<h3 id="text-7" contenteditable="true">生活难免有风风雨雨</h3>
+				<img src='img/iali6.jpg' alt="生活难免有风风雨雨" />
+			</div>
+			<div id="/section-2/page-5" class="ft-page page-9" data-id="page-5">
+				<h3 id="text-8" contenteditable="true">要比自由还自在  </h3>
+				<img src='img/iali7.jpg' />
+			</div>
+			<div id="/section-2/page-6" class="ft-page page-10" data-id="page-6">
+				<h3 id="text-9" contenteditable="true">要比无邪还无害</h3>
+				<img src='img/iali8.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-3" data-id="section-3">
+			<div id="/section-3/page-1" class="ft-page page-11 full-img" data-id="page-1">
+				<h3 id="text-10" contenteditable="true">暮冬时烤雪 迟夏写长信 </h3>
+				<img src='img/iali49.jpg'  />
+			</div>
+			<div id="/section-3/page-2" class="ft-page page-12" data-id="page-2">
+				<h3 id="text-11" contenteditable="true">早春不过 一棵树</h3>
+				<img src='img/iali10.jpg' />
+			</div>
+			<div id="/section-3/page-3" class="ft-page page-13" data-id="page-3">
+				<img src='img/iali11.jpg' alt='可是小明的爱情又在哪里呢？' />
+				<h3 id="text-13" contenteditable="true">山阴路的夏天</h3>
+			</div>
+			<div id="/section-3/page-4" class="ft-page page-14 left-img" data-id="page-4">
+				<h3 id="text-14" contenteditable="true">多么想念你走在我身边的样子 </h3>
+				<img src='img/iali571.jpg' />
+			</div>
+			<div id="/section-3/page-5" class="ft-page page-15 left-img" data-id="page-5">
+				<h3><span id="text-15" contenteditable="true">想起来我的爱就不能停止</span><br /><span id="text-16" contenteditable="true"></h3>
+				<img src='img/iali13.jpg' />
+			</div>
+			<div id="/section-3/page-6" class="ft-page page-16 left-img" data-id="page-6">
+				<h3><span id="text-17" contenteditable="true">跋山涉水后等待</span><br /><span id="text-18" contenteditable="true">我永远在这里，涌着爱面朝沧海</span></h3>
+				<img src='img/iali582.jpg' />
+			</div>
+			<div id="/section-3/page-7" class="ft-page page-17 top-text" data-id="page-7">
+				<h3 id="text-19" contenteditable="true">我从远方赶来，恰巧你也在</h3>
+				<img src='img/iali15.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-4" data-id="section-4">
+			<div id="/section-4/page-1" class="ft-page page-18 full-img" data-id="page-1">
+				<p class="text" id="text-20" contenteditable="true">命中注定的相遇 都失之交臂</p>
+				<img src='img/iali44.jpg'  />
+			</div>
+			<div id="/section-4/page-2" class="ft-page page-19" data-id="page-2">
+				<h3 id="text-21" contenteditable="true">求你别轻言离去 因知音难觅</h3>
+				<img src="img/iali17.jpg" />
+			</div>
+			<div id="/section-4/page-3" class="ft-page page-20" data-id="page-3">
+				<h3 id="text-22" contenteditable="true">我已经开始苍老</h3>
+				<img src='img/iali582.jpg' />
+			</div>
+			<div id="/section-4/page-4" class="ft-page page-21" data-id="page-4">
+				<h3 id="text-23" contenteditable="true">  因为爱过了你</h3>
+				<img src='img/iali61.jpg' />
+			</div>
+			<div id="/section-4/page-5" class="ft-page page-22 left-img" data-id="page-5">
+				<h3 id="text-24" contenteditable="true">爱恋中每一个瞬间都可能就是一生 </h3>
+				<img src='img/iali19.jpg' />
+			</div>
+			<div id="/section-4/page-6" class="ft-page page-23 left-img" data-id="page-6">
+				<h3 id="text-25" contenteditable="true">时光都已经不再 </h3>
+				<img src='img/iali20.jpg' />
+			</div>
+			<div id="/section-4/page-7" class="ft-page page-24 full-img" data-id="page-7">
+				<h3 id="text-26" contenteditable="true">你比我更永恒 </h3>
+				<img src='img/iali21.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-5" data-id="section-5">
+			<div id="/section-5/page-1" class="ft-page page-25 left-img" data-id="page-1">
+				<h3><span id="text-27" contenteditable="true"></span><br /><span id="text-28" contenteditable="true">亲爱的没有了你就没有任何人 </span></h3>
+				<img src='img/iali22.jpg' />
+			</div>
+			<div id="/section-5/page-2" class="ft-page page-26 top-text" data-id="page-2">
+				<h3 id="text-29" contenteditable="true">il y a longtemps que je t'aime</h3>
+				<img src='img/iali162.jpg' />
+			</div>
+			<div id="/section-5/page-3" class="ft-page page-27" data-id="page-3">
+				<h3 id="text-30" contenteditable="true">jamais je ne t'oublierai </h3>
+				<img src='img/iali23.jpg'  />
+			</div>
+			<div id="/section-5/page-4" class="ft-page page-28" data-id="page-4">
+				<h3 id="text-31" contenteditable="true"></h3>
+				<img src='img/iali24.jpg' />
+			</div>
+			<div id="/section-5/page-5" class="ft-page page-29" data-id="page-5">
+				<h3 id="text-32" contenteditable="true">尚好的青春都是你 再遥远 都跟随你</h3>
+				<img src='img/iali55.jpg' />
+			</div>
+			<div id="/section-5/page-6" class="ft-page page-30" data-id="page-6">
+				<h3 id="text-33" contenteditable="true">若滂沱大雨 不曾见证 海角相偎依</h3>
+				<img src='img/iali26.jpg' />
+			</div>
+			<div id="/section-5/page-7" class="ft-page page-31" data-id="page-7">
+				<h3 id="text-34" contenteditable="true">路程再多遥远不要不回来 </h3>
+				<img src='img/iali27.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-6" data-id="section-6">
+			<div id="/section-6/page-1" class="ft-page page-32" data-id="page-1">
+				<h3 id="text-35" contenteditable="true">千万记得天涯有人在等你</h3>
+				<img src='img/iali46.jpg'  />
+			</div>
+			<div id="/section-6/page-2" class="ft-page page-33 top-text" data-id="page-2">
+				<h3 id="text-36" contenteditable="true">请与我同行</h3>
+				<img src='img/iali29.jpg' />
+			</div>
+			<div id="/section-6/page-3" class="ft-page page-34 left-img" data-id="page-3">
+				<h3 id="text-37" contenteditable="true">划起一叶咿哑的扁舟 去探索那遥远的海港</h3>
+				<img src='img/iali37.jpg' />
+			</div>
+			<div id="/section-6/page-4" class="ft-page page-35" data-id="page-4">
+				<h3><span id="text-38" contenteditable="true">任凭风如丧钟 </span><br /><span id="text-39" contenteditable="true">雾似飞网</span><br /><span id="text-40" contenteditable="true">我们一样，就敢在泥沼里 挽起朝晖 走向远方</span></h3>
+				<img src='img/iali30.jpg' />
+			</div>
+			<div id="/section-6/page-5" class="ft-page page-36 full-img" data-id="page-5">
+				<h3 id="text-41" contenteditable="true">我多么希望，有一个门口</h3>
+				<img src='img/iali31.jpg' />
+			</div>
+			<div id="/section-6/page-6" class="ft-page page-37 left-img" data-id="page-6">
+				<h3 id="text-42" contenteditable="true">早晨，阳光照在草上</h3>
+				<img src='img/iali102.jpg' />
+			</div>
+			<div id="/section-6/page-7" class="ft-page page-38 bottom-text" data-id="page-7">
+				<img src='img/iali33.jpg' />
+				<h3 id="text-43" contenteditable="true">我们站着</h3>
+			</div>
+		</div>
+		<div class="ft-section section-7" data-id="section-7">
+			<div id="/section-7/page-1" class="ft-page page-39 left-img" data-id="page-1">
+				<h3 id="text-44" contenteditable="true">扶着自己的门扇</h3>
+				<img src='img/iali34.jpg' />
+			</div>
+			<div id="/section-7/page-2" class="ft-page page-40" data-id="page-2">
+				<h3 id="text-45" contenteditable="true">门很低，但太阳是明亮的</h3>
+				<img src='img/iali35.jpg'  />
+			</div>
+			<div id="/section-7/page-3" class="ft-page page-41" data-id="page-3">
+				
+				<img src='img/iali36.jpg' />
+				<h3 id="text-46" contenteditable="true"> 草在结它的种子</h3>
+			</div>
+			<div id="/section-7/page-4" class="ft-page page-42" data-id="page-4">
+				<h3><span id="text-47" contenteditable="true">风在摇它的叶子</span><br /><span id="text-48" contenteditable="true">我们站着，不说话 就十分美好 </span></h3>
+				<img src='img/iali101.jpg'  />
+			</div>
+			<div id="/section-7/page-5" class="ft-page page-43" data-id="page-5">
+				<img src='img/iali38.jpg' />
+				<h3 id="text-49" contenteditable="true">我们的火堆里什么都有</h3>
+			</div>
+			<div id="/section-7/page-6" class="ft-page page-44 left-img" data-id="page-6">
+				<h3><span id="text-50" contenteditable="true">有松果，有葡萄枝</span><br /><span id="text-51" contenteditable="true">还有赛过流水的鲜花</span></h3>
+				<img src='img/iali39.jpg' />
+			</div>
+			<div id="/section-7/page-7" class="ft-page page-45 left-img" data-id="page-7">
+				<h3><span id="text-52" contenteditable="true">有泥浆也有露滴</span><br /><span id="text-53" contenteditable="true"></span></h3>
+				<img src='img/iali40.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-8" data-id="section-8">
+			<div id="/section-8/page-1" class="ft-page page-46" data-id="page-1">
+				<img src='img/iali41.jpg' />
+				<h3>
+					<span id="text-54" contenteditable="true">长安月下</span>
+					<span id="text-55" contenteditable="true">一壶清酒</span>
+					<span id="text-56" contenteditable="true">一树桃花</span>
+					<span id="text-57" contenteditable="true">我们谈啊谈啊 犹如梦中</span>
+				</h3>
+			</div>
+			<div id="/section-8/page-2" class="ft-page page-47 top-text" data-id="page-2">
+				<img src='img/iali42.jpg' />
+				<h3>
+					<span id="text-58" contenteditable="true">而今你属于我 于我倚梦而憩</span>
+					<span id="text-59" contenteditable="true">爱与苦现在都该安眠了</span>
+					<span id="text-60" contenteditable="true">你在我身旁</span>
+					<span id="text-61" contenteditable="true">纯净如一熟睡的琥珀</span></h3>
+
+			</div>
+			<div id="/section-8/page-3" class="ft-page page-48 left-img" data-id="page-3">
+				<p><span id="text-62" contenteditable="true">我想和你虚度时光，比如低头看鱼</span><br /><span id="text-63" contenteditable="true">比如把茶杯留在桌子上</span></p>
+				<img src='img/iali43.jpg'  />
+			</div>
+			<div id="/section-8/page-4" class="ft-page page-49 full-img" data-id="page-4">
+			<img src='img/iali100.jpg' />
+				<p class="text">
+					<span class='text' id="text-64" contenteditable="true">我还想连落日一起浪费</span><br />
+					<span class='text' id="text-65" contenteditable="true">比如散步</span>
+				</p>
+			</div>
+			<div id="/section-8/page-5" class="ft-page page-50" data-id="page-5">
+				<p class='text' id="text-66" contenteditable="true">我还想连落日一起浪费 比如散步 一直消磨到星光满天 </p>
+				<img src='img/iali103.jpg' />
+			</div>
+			<div id="/section-8/page-6" class="ft-page page-51" data-id="page-6">
+				<p>
+					<span id="text-67" contenteditable="true">我还要浪费风起的时候</span><br />
+					<span id="text-68" contenteditable="true">坐在走廓发呆，直到你眼中乌云</span><br />
+					<span id="text-69" contenteditable="true">全部被吹到窗外 </span><br />
+					<span id="text-70" contenteditable="true">一起消磨精致而苍老的宇宙 </span>
+					</p>
+				<img src='img/iali59.jpg' />
+			</div>
+		</div>
+		<div class="ft-section section-9" data-id="section-9">
+			<div id="/section-9/page-1" class="ft-page page-52 full-img" data-id="page-1">
+				<h3 id="text-71" contenteditable="true">直到所有被虚度的事物 在我们身后，长出薄薄的翅膀</h3>
+				<img src='img/iali28.jpg'  />
+			</div>
+			<div id="/section-9/page-2" class="ft-page page-53" data-id="page-2">
+				<img src='img/iali47.jpg' alt='happy birthday' />
+				<h3 id="text-72" contenteditable="true">Love forever</h3>
+			</div>
+			<div id="/section-9/page-3" class="ft-page page-54 center-img" data-id="page-3" >
+				<div class="center-img">I love you</div>
+			</div>
+			<div id="/section-9/page-4" class="ft-page page-55 right-img" data-id="page-4">
+				<img src='img/14915.jpg' />
+				<p class="text">
+					<span id="text-73" contenteditable="true">2017.05.20  04:47:35 The 5.20 gift for you</span><br />
+					<span id="text-74" contenteditable="true">谢谢你 让我变得更好---Mr.Sun</span><br />
+					<br />
+					<br />
+					<span> Surprise </span>
+				</p> 
+			</div>
+		</div>
+	</div>
+
+
+	<audio id="bgmMusic" src="music/music.mp3" autoplay="autoplay" loop preload="auto" type="audio/mp3"></audio>
+    <div class="mPower"><span id="on" title="点击暂停"></span><span id="off" title="点击播放"></span></div>
+
+	<script src="js/jquery.min.js"></script>
+	<script src="js/brav1toolbox.js"></script>
+	<script src="js/flowtime.js"></script>
+	<script src="js/love.js"></script>
+</body>
+</html>
